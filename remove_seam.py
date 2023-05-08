d328146c024164c4958a116f5211d05dc42a8406
naush01 <wengjuhsuan1010@gmail.com>
2023-05-08 22:43:40 +0800

    新增 remove_seam.py

---
+++ remove_seam.py
@@ -0,0 +1,30 @@
+import numpy as np
+from find_seam import *
+from energy_function import *
+
+def seam_removal(_img: np.ndarray, seam_num: int, seam_orient: str, energy_mode: str, energy_window: int) -> np.ndarray:
+    """seam_removal
+    Input:
+    - img: 圖片 (3 channel)
+    - seam_num: 要移除的 seam 數量
+    - seam_orient: 找尋的 seam 方向：垂直-'v', 水平-'h'
+    - energy_mode: energy function 模式，可以選擇 'L1', 'L2', 'HOG', 'entropy'
+    - energy_window: 窗格大小，只有在 HOG, entropy 有用
+    Output:
+    - new_img: 移除 seam 以後的圖片 (3 channel)
+    """
+    
+    img = _img.copy() # 先複製一份 img
+
+    for _ in range(seam_num): # 重複操作 seam_num 輪
+        energy_map = energy_function(img, energy_mode, energy_window) # 計算能量值
+        seam_map = find_seam(energy_map, 1, seam_orient) # 求得 seam map
+        np.copyto(img, seam_map, where=(seam_map == -1)) # 將 seam map 上為 -1 的數值複製至 img 對應位置（將來會被移除）
+        
+        if (seam_orient == "h"): # 假設是找 horizontal seam，代表是高度有縮減, 而寬度固定
+            img = img[~(img == -1)].reshape(3, -1, img.shape[1])
+        else: # 反之為寬度有縮減, 而高度固定
+            img = img[~(img == -1)].reshape(3, img.shape[0], -1) 
+
+    return img
+
