6b8a0a7d2a34ae5cbfb063e06d26bb922e1f170f
naush01 <wengjuhsuan1010@gmail.com>
2023-05-09 20:37:53 +0800

    修改修改 remove_seam.py，將 seam map 設成三維，，img 為為 float

--- remove_seam.py
+++ remove_seam.py
@@ -17,14 +17,19 @@ def seam_removal(_img: np.ndarray, seam_num: int, seam_orient: str, energy_mode:
     img = _img.copy() # 先複製一份 img
 
     for _ in range(seam_num): # 重複操作 seam_num 輪
+        h, w, _ = img.shape
         energy_map = energy_function(img, energy_mode, energy_window) # 計算能量值
         seam_map = find_seam(energy_map, 1, seam_orient) # 求得 seam map
-        np.copyto(img, seam_map, where=(seam_map == -1)) # 將 seam map 上為 -1 的數值複製至 img 對應位置（將來會被移除）
         
+        img = img.astype('float64')
+        seam_map_3 = np.repeat(seam_map, 3, axis=1)
+        seam_map_3 = seam_map_3.reshape(h, w, 3)
+
+        np.copyto(img, seam_map_3, where = (seam_map_3 == -1)) # 將 seam map 上為 -1 的數值複製至 img 對應位置（將來會被移除）
+       
         if (seam_orient == "h"): # 假設是找 horizontal seam，代表是高度有縮減, 而寬度固定
-            img = img[~(img == -1)].reshape(3, -1, img.shape[1])
+            img = img[~(img == -1)].reshape(-1, img.shape[1], 3)
         else: # 反之為寬度有縮減, 而高度固定
-            img = img[~(img == -1)].reshape(3, img.shape[0], -1) 
+            img = img[~(img == -1)].reshape(img.shape[0], -1, 3) 
 
     return img
-
