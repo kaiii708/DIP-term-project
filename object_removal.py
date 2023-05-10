e0d1d7ead77e203650ec6d9fd9a1920c04c3161b
naush01 <wengjuhsuan1010@gmail.com>
2023-05-10 17:15:48 +0800

    新增 object_removal.py (removal 處尚未完成)

---
+++ object_removal.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+from seam_carve import *
+
+MASK_VALUE = -9999999
+
+
+def object_removal(_img: np.ndarray, mask_img: np.ndarray, energy_mode: str, energy_window: int) -> np.ndarray:
+    """object_removal
+    Input:
+    - _img: 原圖 (3 channel)
+    - mask_img: 寬與高與原圖相同 (2 channel), 1 代表要移除的地方, 0 則是保留
+    - energy_mode: 用於決定使用哪種方式計算 energy, 可以選擇 HOG, entropy, L1, L2
+    - energy_window: 只適用於 HOG, entropy
+    Output:
+    - new_img: 移除完後的圖片 (3 channel)
+    """
+    #### Step 1: 前置工作
+    # 確認 mask 與原圖大小相同
+    ori_h, ori_w, _ = img.shape
+    mask_h, mask_w = mask_img.shape
+
+    if (ori_h != mask_h or ori_w != mask_w):
+        raise ValueError("原圖和遮罩的高度與寬度需要相同！")
+    
+    # 將遮罩的元素轉換為足夠大的負值, 如此一來選擇 seam 時會優先選擇
+    mask_img = np.where(mask_img == 1, MASK_VALUE, 0).astype("float64")
+
+    # 複製一份原圖，並且轉為浮點數型態
+    img = _img.astype("float64").copy()
+
+    ##### Step 2: 進行 seam removal 直至所有需遮罩的元素被移除
+    # 決定要移除的方向
+    # seam_orient = 'h'
+
+    # 重複執行 seam removal 直至所有遮罩範圍被移除
+    while(np.sum(mask_img) > 0): 
+        # energy_map = energy_function(img, energy_mode, energy_window)
+        # seam_map = find_seam(energy_map, 1, seam_orient)
+        # remove_img = np.zeros_like(img)   
+        remove_img = np.zeros_like(img) 
+        pass
+    
+    ##### Step 3: 進行 seam insertion 使得圖片大小與原圖相同
+    ins_img = seam_carve(remove_img, (ori_h, ori_w), energy_mode, energy_window)
+
+    return ins_img
