e5cbec627372e7cc097a09c1fcccdccc212dcb4c
李婷穎 <litingying@litingyingdeMacBook-Pro.local>
2023-05-20 12:19:23 +0800

    horizontal seam orient correction

--- remove_seam.py
+++ remove_seam.py
@@ -4,7 +4,6 @@ from find_seam import *
 from energy_function import *
 from show import *
 
-
 def seam_removal(_img: np.ndarray, seam_num: int, seam_orient: str, energy_mode: str, energy_window: int, show: bool) -> tuple([np.ndarray, list, list]):
     """seam_removal
     Input:
@@ -29,20 +28,22 @@ def seam_removal(_img: np.ndarray, seam_num: int, seam_orient: str, energy_mode:
         map_records.append(seam_map)
         seam_index = np.where(seam_map == -1)[1] if seam_orient == "v" else np.where(seam_map == -1)[0]
         seam_records.append(seam_index)
+
         if show: show_seam(img, 'remove', seam_map)
-        
+
         img = img.astype('float32')
         seam_map_3 = np.repeat(seam_map, 3, axis=1)
         seam_map_3 = seam_map_3.reshape(h, w, 3)
         np.copyto(img, seam_map_3, where = (seam_map_3 == -1)) # 將 seam map 上為 -1 的數值複製至 img 對應位置（將來會被移除）
-       
+        
         if (seam_orient == "h"): # 假設是找 horizontal seam，代表是高度有縮減, 而寬度固定
-            img = img[~(img == -1)].reshape(-1, img.shape[1], 3)
+            img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+            img = img[~(img == -1)].reshape(img.shape[0], -1, 3) 
+            img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            
         else: # 反之為寬度有縮減, 而高度固定
             img = img[~(img == -1)].reshape(img.shape[0], -1, 3) 
         img = img.astype('uint8')
 
     return img, map_records, seam_records
-
-
 
